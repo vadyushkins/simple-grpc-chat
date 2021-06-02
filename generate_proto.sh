d34bda5f16747da983eb3efd84c5afa644982516
vdshk <vadim.i.abzalov@gmail.com>
2021-06-02 16:49:24 +0300

    Removed generate_proto.sh

--- generate_proto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Generating proto gRPC files..."
-python3 -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. chat.proto
-echo "Generating proto gRPC files... DONE"
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import client
-import server
+import os
+
+if not os.path.exists("chat_pb2.py") or not os.path.exists("chat_pb2_grpc.py"):
+    os.system(
+        (
+            "python3 "
+            "-m "
+            "grpc_tools.protoc "
+            "-I=. "
+            "--python_out=. "
+            "--grpc_python_out=. "
+            "chat.proto "
+        )
+    )
+
+from client import run as run_client
+from server import run as run_server
 
 
 def main():
@@ -9,10 +24,10 @@ def main():
 
     if type == "Server":
         print("Starting server...")
-        server.run()
+        run_server()
     elif type == "Client":
         print("Starting client...")
-        client.run()
+        run_client()
 
 
 if __name__ == "__main__":
