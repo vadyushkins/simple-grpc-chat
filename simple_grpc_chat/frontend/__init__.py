8bc9e7a7925a5574b5b86d0a71cb9a78ee14a8db
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2021-06-07 18:43:57 +0300

    Added frontend/ui

---
+++ simple_grpc_chat/frontend/__init__.py
@@ -0,0 +1,2 @@
+from simple_grpc_chat.frontend.config import *
+from simple_grpc_chat.frontend.src import *
---
+++ simple_grpc_chat/frontend/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+__all__ = [
+    "FRONTEND_DIR",
+]
+
+FRONTEND_DIR = Path(__file__).parent
